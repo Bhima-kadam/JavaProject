0a3765cd3fedd7d12e19375b8a0827b590c22414
Bhima-kadam <kadambhimambika@gmail.com>
2025-10-14 12:32:05 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,8 @@
 class Sample {
  public static void main(String args[]) {
 System.out.println("Hello world");
+  System.out.println("Welcome to Devops");
+  System.out.println("Welcome to Jenkins");
+  
 }
 }
