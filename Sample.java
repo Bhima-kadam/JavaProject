4335c3108c6aac98246a30d21929caf9595eadf0
Bhima-kadam <kadambhimambika@gmail.com>
2025-10-25 16:44:55 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ class Sample {
  public static void main(String args[]) {
   System.out.println("Welcome to Devops");
   System.out.println("Welcome to Jenkins");
-  System.out.println("welcome to Git");
+  System.out.println("welcome to Git..");
   
 }
 }
